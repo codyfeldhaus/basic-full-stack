a6413e0551c01bde1b6aba32b40502d038bbdf09
Cody Feldhaus <cody.feldhaus@gmail.com>
2023-12-27 20:46:28 -0600

    front end started and files cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+//useState - search term, results
+//map results to our list
+//key (item id)
+//onChange for the search term
+
+import React, { useState } from 'react';
 
 function App() {
+  //create state variables for searchTerm and results
+  const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  //handler for onClick
+  const handleSearch = async () => {
+    //use fetch to call server API to search DB for given last name
+    const response = await fetch(`http://localhost:3001/search?q=${searchTerm}`);
+    //convert the response to json and store in variable
+    const data = await response.json();
+    //set the results to be the json data
+    setResults(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Basic Full Stack - Last Name DB Search</h1>
+
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {/* onClick for button */}
+      <button onClick={handleSearch}>Search</button> 
+
+      <h2>Results</h2>
+      <ul>
+        {/* Code to render results here */}
+        {results.map(result => (
+          <li key={result.id}>{result.first_name ? result.first_name : "NULL"} {result.last_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
